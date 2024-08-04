1b5150a00714cb353adf16e44eeea7a8031921d6
hala21 <lizhiyong567@gmail>
2024-08-04 23:01:08 +0800

    cpp done

--- exercises/29_std_accumulate/main.cpp
+++ exercises/29_std_accumulate/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size =
-    ASSERT(size == 602112, "4x1x3x224x224 = 602112");
+    int size = std::accumulate(shape, shape+4, 1, [](int a, int b) -> int {return a + b;});
+    ASSERT(size = 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
